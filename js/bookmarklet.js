0b578615fd68e5d5722c0702d31bef3423926d0c
fleidloff <frederik.leidloff@student.hpi.uni-potsdam.de>
2013-07-10 11:41:12 +0200

    added modified soundcloud sdk version that uses HTML5 audio!

---
+++ js/bookmarklet.js
@@ -0,0 +1,18 @@
+var Request = {	
+	parameter: function(name) {
+		return this.parameters()[name];
+	},
+	
+	parameters: function() {
+		var result = {};
+		var url = window.location.href;
+		var parameters = url.slice(url.indexOf('?') + 1).split('&');
+		
+		for(var i = 0;  i < parameters.length; i++) {
+			var parameter = parameters[i].split('=');
+			result[parameter[0]] = parameter[1];
+		}
+		return result;
+	}
+};
+location.href='https://dl.dropboxusercontent.com/u/5100494/SC-SlowDowner/index.html#' + window.location.href;
